a50ef527aa911fca335ea5d1376291348d744fd4
Abdulraheem Sherif Adavuruku <abdulraheem.sherif@outsourceglobal.com>
2020-05-11 16:32:52 +0100

    start

---
+++ app/src/main/java/com/example/blooddonor/dbColumnList.java
@@ -0,0 +1,42 @@
+package com.example.blooddonor;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by sherif146 on 03/01/2018.
+ */
+
+public class dbColumnList {
+    public static class usersRecord implements BaseColumns{
+        public static final String TABLE_NAME = "usersRecord";
+        public static final String COLUMN_FULLNAME = "fullname";
+        public static final String COLUMN_GENDER = "gender";
+        public static final String COLUMN_PHONE = "phone";
+        public static final String COLUMN_EMAIL = "email";
+        public static final String COLUMN_BLOODTYPE= "bloodtype";
+        public static final String COLUMN_STATE = "userstate";
+        public static final String COLUMN_LGOV= "userlocalgov";
+        public static final String COLUMN_USERTYPE = "usertype";
+        public static final String COLUMN_CONTACTADD = "contactAddress";
+    }
+
+    public static class hospitalBankBlood implements BaseColumns{
+        public static final String TABLE_NAME = "hospitalBankBlood";
+        public static final String COLUMN_PHONE = "phone";
+        public static final String COLUMN_BLOODTYPE= "bloodtype";
+        public static final String COLUMN_QUANTITY = "quantity";
+    }
+
+    public static class usersRequest implements BaseColumns{
+        public static final String TABLE_NAME = "usersRequest";
+        public static final String COLUMN_PHONE = "phone";
+        public static final String COLUMN_STATE = "userstate";
+        public static final String COLUMN_LOCALGOV = "userlocalgov";
+        public static final String COLUMN_REQUESTTYPE= "requesttype";
+        public static final String COLUMN_BLOODTYPE= "bloodtype";
+        public static final String COLUMN_REQUESTSTATUS = "requeststatus";
+        public static final String COLUMN_UNIT = "unit";
+        public static final String COLUMN_CONTACTADD = "contactAddress";
+        public static final String COLUMN_DATE = "recordDate";
+    }
+}
